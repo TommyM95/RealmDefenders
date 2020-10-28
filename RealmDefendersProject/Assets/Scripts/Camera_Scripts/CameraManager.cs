e0dbb71840e313764fde2ab32a1be8a9cdd12357
Tommy <mcgillt95@gmail.com>
2020-10-28 18:04:05 +0000

    Added Cinemachine
    Added Cinemachine package to better control the camera via code, without having to write as much complex camera code.  At the moment you can move the camera via W,A,S,D or arrow keys and Zoom in and out with the scroll wheel

---
+++ RealmDefendersProject/Assets/Scripts/Camera_Scripts/CameraManager.cs
@@ -0,0 +1,51 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+
+public class CameraManager : MonoBehaviour
+{
+    [SerializeField] float cameraMoveSpeed = 5f;
+    [SerializeField] float zoomAmount = 2f;
+    [SerializeField] float zoomSpeed = 2f;
+    [SerializeField] private CinemachineVirtualCamera cinemachineVirtualCamera = null;
+    [SerializeField] float minOrthgraphicSize = 10f;
+    [SerializeField] float maxOrthgraphicSize = 30f;
+
+    private float orthgraphicSize;
+    private float targetOrthgraphicSize;
+
+    private void Start()
+    {
+        orthgraphicSize = cinemachineVirtualCamera.m_Lens.OrthographicSize;
+        targetOrthgraphicSize = orthgraphicSize;
+    }
+
+    private void LateUpdate()
+    {
+        CameraMove();
+        CameraZoom();
+    }
+
+    private void CameraMove()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveDirection = new Vector3(x, y).normalized;
+
+        transform.position += moveDirection * cameraMoveSpeed * Time.deltaTime;
+    }
+
+    private void CameraZoom()
+    {
+        targetOrthgraphicSize += -Input.mouseScrollDelta.y * zoomAmount;
+        targetOrthgraphicSize = Mathf.Clamp(targetOrthgraphicSize, minOrthgraphicSize, maxOrthgraphicSize);
+
+        orthgraphicSize = Mathf.Lerp(orthgraphicSize, targetOrthgraphicSize, Time.deltaTime * zoomSpeed);
+
+        cinemachineVirtualCamera.m_Lens.OrthographicSize = orthgraphicSize;
+    }
+}
