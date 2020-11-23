c89f8c26cb58f54f6632abf89e4cae457dc575b4
Tommy <mcgillt95@gmail.com>
2020-11-23 23:39:34 +0000

    Added the ability to remove placed buildings

--- RealmDefendersProject/Assets/Scripts/Building_Scripts/Building.cs
+++ RealmDefendersProject/Assets/Scripts/Building_Scripts/Building.cs
@@ -7,6 +7,13 @@ public class Building : MonoBehaviour
 {
     private so_BuildingType buildingType;
     private HealthSystem healthSystem;
+    private Transform buildingDemolishButton;
+
+    private void Awake()
+    {
+        buildingDemolishButton = transform.Find("pf_BuildingDemolishButton");
+        HideDemolishButton();
+    }
 
     private void Start()
     {
@@ -19,5 +26,31 @@ public class Building : MonoBehaviour
     private void HealthSystem_OnDied(object sender, EventArgs e)
     {
         Destroy(gameObject);
+    }
+
+    private void OnMouseEnter()
+    {
+        ShowDemolishButton();
+    }
+
+    private void OnMouseExit()
+    {
+        HideDemolishButton();
+    }
+
+    private void ShowDemolishButton()
+    {
+        if (buildingDemolishButton != null)
+        {
+            buildingDemolishButton.gameObject.SetActive(true);
+        }
+    }
+
+    private void HideDemolishButton()
+    {
+        if (buildingDemolishButton != null)
+        {
+            buildingDemolishButton.gameObject.SetActive(false);
+        }
     }
 }
